bdf14659575233d49d522c86e081a6a11e7c8e0e
jiayaoli0314 <li.jiayao@northeastern.edu>
2021-12-05 21:12:48 -0800

    add storeTXT()

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,16 +4,23 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClients;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
-    public static void main(String[] args){
+    public static MongoClient mongoClient;
+    public static void main(String[] args) throws Exception {
         try {
             Dotenv dotenv = Dotenv.load();
             ConnectionString connectionString = new ConnectionString("mongodb+srv://6650final:" + dotenv.get("DBPASSWORD") + "@cluster0.kuupt.mongodb.net/defaultDatabase?retryWrites=true&w=majority");
             MongoClientSettings settings = MongoClientSettings.builder()
                     .applyConnectionString(connectionString)
                     .build();
-            MongoClient mongoClient = MongoClients.create(settings);
+            mongoClient = MongoClients.create(settings);
             MongoDatabase database = mongoClient.getDatabase("defaultDatabase");
             System.out.println("Connect to database successfully");
 
@@ -24,5 +31,34 @@ public class Database {
         } catch (Exception e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
         }
+
+        storeTXT("defaultDatabase", "testCollection", "/Users/yvetteli/Desktop/ReadMe.txt");
     }
+
+    public static void storeTXT(String MongoDBName, String MongoDBCollection, String filePath) throws Exception {
+        if (!filePath.endsWith("txt")) {
+            throw new Exception("file is not txt");
+        }
+        MongoDatabase db = mongoClient.getDatabase(MongoDBName);
+        MongoCollection<Document> collection = db.getCollection(MongoDBCollection);
+
+        File file = new File(filePath);
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString;
+            String wholeString = "";
+            while ((tempString = reader.readLine()) != null) {
+                wholeString += tempString;
+            }
+            reader.close();
+            Document document = new Document();
+            document.append("txt", wholeString);
+            collection.insertOne(document);
+            System.out.println("read txt successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
